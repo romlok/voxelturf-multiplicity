07542f54cf91455f9f8bf033e62638919cab6b92
Mel Collins <mel@raumkraut.net>
2017-11-01 14:14:32 +0100

    Put multiplicity sheen on mod

---
+++ scripts/multiplicity.lua
@@ -0,0 +1,14 @@
+-- Overrides calls to generate_city with multiple calls to generate_city
+
+function define_multiple_cities ()
+	if (generate_city_old == nil) then -- Prevents double-defines
+		generate_city_old = generate_city; -- backup existing generate_city
+		
+		generate_city = function (W, LC, radius, startX, startZ, maxPlayerBases) -- define a wrapper
+			return generate_city_old (W, LC, radius, startX, startZ, maxPlayerBases);
+		end
+	end
+end
+
+if (defineBlocksUserCallback == nil) then defineBlocksUserCallback = {}; end
+defineBlocksUserCallback[#defineBlocksUserCallback + 1] = { "define_multiple_cities|Injection point for MultipliCity mod", define_multiple_cities } 
